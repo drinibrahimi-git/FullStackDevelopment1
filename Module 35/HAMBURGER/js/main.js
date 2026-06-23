962c295103e2e2c627a50fb886d39cb5d941ea7d
drinibrahimi-git <edona.beshiri1988@gmail.com>
2026-06-23 20:40:38 +0200

    lesson 36

---
+++ Module 35/HAMBURGER/js/main.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    var menu = document.getElementById("myLinks");
+    if(menu.style.displa === "block") {
+        menu.style.display = "none"
+    } else {
+        menu.style.display = "block"
+    }
+}
